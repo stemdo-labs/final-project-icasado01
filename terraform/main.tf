67db0e567eb5ec320b410ca8c97835fe3972c907
icasado01 <166407751+icasado01@users.noreply.github.com>
2024-06-18 20:53:14 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 backend "azurerm" {
-  resource_group_name  = "rg-plopez-dvfinlab"
-  storage_account_name = "staplopezdvfinlab"
+  resource_group_name  = "rg-icasado-dvfinlab"
+  storage_account_name = "staicasadodvfinlab"
   container_name       = "tfstate"
   key                  = "terraform.tfstate"
 }
